89cdb23c69949be708e2b2637cd8f8077f7e208a
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-28 23:23:00 +0000

    automatically backed up by learn

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,17 @@
 class GamesController < ApplicationController
-  # Add your GamesController code here
+  def new
+    @game = Game.new
+  end
+
+  def create
+    @game = Game.create(game_params)
+    render json: @game, status: 201
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit()
+  end
 
 end
